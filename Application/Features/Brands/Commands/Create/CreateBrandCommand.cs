055146fe7dc73ba8fc07b51012a477987b0609f6
huseyinaydin99 <huseyinaydin99@gmail.com>
2024-02-29 12:02:41 +0300

    Add project files.

---
+++ Application/Features/Brands/Commands/Create/CreateBrandCommand.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Brands.Commands.Create;
+
+public class CreateBrandCommand : IRequest<CreatedBrandResponse>
+{
+    public string Name { get; set; }
+    public class CreateBrandCommandHandler : IRequestHandler<CreateBrandCommand, CreatedBrandResponse>
+    {
+        public async Task<CreatedBrandResponse>? Handle(CreateBrandCommand request, CancellationToken cancellationToken)
+        {
+            CreatedBrandResponse response = new();
+            response.Name = request.Name;
+            response.Id = new Guid();
+            return response;
+        }
+    }
+}
