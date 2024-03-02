48909bd6ab301324521fcccb4dda1dc7b9ec745b
huseyinaydin99 <huseyinaydin99@gmail.com>
2024-03-02 13:20:23 +0300

    Persistence Layer yani kalıcılık katmanı Brand Entity'si için tamamlandı. PersistenceServiceRegistration configuration tamamlandı. Her bir katmanın IoC configuration'u kendine ait.

---
+++ Persistance/Contexts/BaseDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistance.Contexts;
+
+public class BaseDbContext : DbContext
+{
+    protected IConfiguration Configuration { get; set; }
+    public DbSet<Brand> Brands { get; set; }
+
+    public BaseDbContext(DbContextOptions options, IConfiguration configuration) : base(options)
+    {
+        Configuration = configuration;
+        Database.EnsureCreated();//Önce veritabanının oluştuğundan emin ol.
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+}
---
+++ Persistance/PersistenceServiceRegistration.cs
@@ -0,0 +1,27 @@
+﻿using Application.Features.Services.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Persistance.Contexts;
+using Persistance.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistance;
+
+public static class PersistenceServiceRegistration
+{
+    public static IServiceCollection AddPersistenceServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        //services.AddDbContext<BaseDbContext>(options => options.UseInMemoryDatabase("nArchitecture"));
+
+        services.AddDbContext<BaseDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("RentACar")));
+
+        services.AddScoped<IBrandRepository, BrandRepository>();
+
+        return services;
+    }
+}
