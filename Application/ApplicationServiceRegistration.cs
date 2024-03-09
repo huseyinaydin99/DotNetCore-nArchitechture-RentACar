141a1007021aabe549e6ecbf5299d0115a27b6c1
huseyinaydin99 <huseyinaydin99@gmail.com>
2024-03-09 20:03:16 +0300

    CreateBrandCommand ve GetListBrandQuery sınıflarına ILoggableRequest interface'i implemente edildi. Bu sayede bu sınıfların içindeki Handle metotları Behavior AOP tekniğiyle loglanacaktır.

--- Application/ApplicationServiceRegistration.cs
+++ Application/ApplicationServiceRegistration.cs
@@ -1,7 +1,10 @@
 ﻿using Core.Application.Pipelines.Caching;
+using Core.Application.Pipelines.Logging;
 using Core.Application.Pipelines.Transaction;
 using Core.Application.Pipelines.Validation;
 using Core.Application.Rules;
+using Core.CrossCuttingConcerns.Serilog.Logger;
+using Core.CrossCuttingConcerns.Serilog;
 using FluentValidation;
 using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
@@ -24,7 +27,10 @@ public static class ApplicationServiceRegistration
             configuration.AddOpenBehavior(typeof(TransactionScopeBehavior<,>));
             configuration.AddOpenBehavior(typeof(CachingBehavior<,>));
             configuration.AddOpenBehavior(typeof(CacheRemovingBehavior<,>));
+            configuration.AddOpenBehavior(typeof(LoggingBehavior<,>));
         });
+        //services.AddSingleton<LoggerServiceBase, MsSqlLogger>();
+        services.AddSingleton<LoggerServiceBase, FileLogger>();
         return services;
     }
 
