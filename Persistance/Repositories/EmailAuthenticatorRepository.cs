bdf0f206190cb8edcf8dc41aecf80bec6fe92cf1
huseyinaydin99 <huseyinaydin99@gmail.com>
2024-03-16 16:35:21 +0300

    Kalan EntityConfiguration ve Repository implementasyonları yapıldı.

---
+++ Persistance/Repositories/EmailAuthenticatorRepository.cs
@@ -0,0 +1,11 @@
+﻿using Application.Services.Repositories;
+using Core.Persistence.Repositories;
+using Core.Security.Entities;
+using Persistance.Contexts;
+
+namespace Persistence.Repositories;
+
+public class EmailAuthenticatorRepository : EfRepositoryBase<EmailAuthenticator, int, BaseDbContext>, IEmailAuthenticatorRepository
+{
+    public EmailAuthenticatorRepository(BaseDbContext context) : base(context) { }
+}
---
+++ Persistance/Repositories/UserRepository.cs
@@ -0,0 +1,11 @@
+﻿using Application.Services.Repositories;
+using Core.Persistence.Repositories;
+using Core.Security.Entities;
+using Persistance.Contexts;
+
+namespace Persistence.Repositories;
+
+public class UserRepository : EfRepositoryBase<User, int, BaseDbContext>, IUserRepository
+{
+    public UserRepository(BaseDbContext context) : base(context) { }
+}
