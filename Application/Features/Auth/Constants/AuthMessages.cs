a573ce2a7bc15df9ec0aa23c3074eee66aaf6ceb
huseyinaydin99 <huseyinaydin99@gmail.com>
2024-03-17 10:55:48 +0300

    CreatedUserCommand için CQRS implementasyonu yapıldı ve iş kuralları yazıldı.

---
+++ Application/Features/Auth/Constants/AuthMessages.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Auth.Constants;
+
+public static class AuthMessages
+{
+    public const string EmailAuthenticatorDontExists = "E-posta kimlik doğrulayıcısı mevcut değil.";
+    public const string OtpAuthenticatorDontExists = "Otp kimlik doğrulayıcısı mevcut değil.";
+    public const string AlreadyVerifiedOtpAuthenticatorIsExists = "Otp kimlik doğrulayıcısı zaten var.";
+    public const string EmailActivationKeyDontExists = "E-posta aktivasyon anahtarı yok.";
+    public const string UserDontExists = "Böyle bir kullanıcı yok.";
+    public const string UserHaveAlreadyAAuthenticator = "Kullanıcının zaten bir kimlik doğrulayıcısı var.";
+    public const string RefreshDontExists = "Yenileme mevcut değil.";
+    public const string InvalidRefreshToken = "Geçersiz yenileme jetonu(hash).";
+    public const string UserMailAlreadyExists = "Bu kullanıcının e-postası zaten var.";
+    public const string PasswordDontMatch = "Şifreler uyuşmuyor.";
+}
